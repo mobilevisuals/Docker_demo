990a23b0476251385d4abc316a98ed294e47de7e
mobilevisuals <hwddheedj63!>
2019-12-09 21:02:20 +0100

    Changing of names.

--- src/main/java/com/example/GreetingsController.java
+++ src/main/java/com/example/GreetingsController.java
@@ -12,12 +12,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/greeting")
 public class GreetingsController {
 
-    @GetMapping(value = {"{account}/types/{type}/ids/{id}", "{account}/types/{type}", "{account}/ids/{id}"})
+    @GetMapping(value = {"{account}/type/{type}/id/{id}", "{account}/type/{type}", "{account}/id/{id}"})
     public String handleGreeting(@PathVariable(required = true) String account, @PathVariable(required = false) String type,
             @PathVariable(required = false) @Min(0) Integer id) {
         switch (account) {
             case "personal":
-                return "Hi userID " + id;
+                return "Hi, userId " + id;
             case "business":
                 if (type == null) {
                     throw new GreetingException("You have to choose a type if you have a business account.");
@@ -25,7 +25,7 @@ public class GreetingsController {
                     switch (type) {
 
                         case "big":
-                            return "Welcome business user! ";
+                            return "Welcome, business user! ";
                         case "small":
                             throw new GreetingException("Path is not yet implemented");
                         default:
